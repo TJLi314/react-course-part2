a534480844aa7b0632cd680995a5713bdfae1dde
TJ Li <TianjianLi314@gmail.com>
2024-05-18 00:40:13 -0500

    done for global state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import { useReducer } from "react";
 import "./App.css";
-import LoginStatus from "./state-management/LoginStatus";
-import TaskList from "./state-management/TaskList";
-import tasksReducer from "./state-management/reducers/tasksReducer";
-import NavBar from "./state-management/NavBar";
+import Counter from "./state-management/counter/Counter";
 import HomePage from "./state-management/HomePage";
-import TasksContext from "./state-management/contexts/tasksContext";
-import AuthReducer from "./state-management/reducers/authReducer";
-import AuthContext from "./state-management/contexts/authContext";
-import AuthProvider from "./state-management/AuthProvider.";
+import NavBar from "./state-management/NavBar";
+import { TasksProvider } from "./state-management/tasks";
 
 function App() {
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-
   return (
     <>
-      <AuthProvider>
-        <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
-          <NavBar />
-          <HomePage />
-        </TasksContext.Provider>
-      </AuthProvider>
+      <TasksProvider>
+        <Counter />
+        <NavBar />
+        <HomePage />
+      </TasksProvider>
     </>
   );
 }
--- src/state-management/tasks/TaskList.tsx
+++ src/state-management/tasks/TaskList.tsx
@@ -1,11 +1,12 @@
-import { useContext, useReducer, useState } from "react";
-import tasksReducer from "./reducers/tasksReducer";
-import TasksContext from "./contexts/tasksContext";
-import AuthContext from "./contexts/authContext";
+import { useContext } from "react";
+import TasksContext from "./tasksContext";
+import useAuthStore from "../auth/store";
+
+const useTasks = () => useContext(TasksContext);
 
 const TaskList = () => {
-  const { tasks, dispatch } = useContext(TasksContext);
-  const { user } = useContext(AuthContext);
+  const { tasks, dispatch } = useTasks();
+  const { user } = useAuthStore();
 
   return (
     <>
